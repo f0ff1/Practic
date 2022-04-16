52066fb0f06d5d3b365249274140e2dc880c7838
f0ff1 <drewcool@mail.ru>
2022-04-16 11:15:55 +0300

    Add Practic 17
    Добавил практику № 17

---
+++ Практика 17/Практика 17/Задание 2 - тестирование библиотеки/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Библиотека_расписаний;
+
+namespace Задание_2___тестирование_библиотеки
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Schedule schedule = new Schedule();
+
+            var mySchedule = schedule.Create();
+            schedule.WriteShedule(mySchedule);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
