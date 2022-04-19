4822392dbf68f9db5552a05acb6f2bab1a6871c6
f0ff1 <drewcool@mail.ru>
2022-04-19 13:20:11 +0300

    Add changet Practic 20
    Добавил практику № 20 с измененными названиями папок

---
+++ Practic_21/Практика 21/Задание 1/Strategy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Задание_1
+{
+    class Strategy
+    {
+        public interface IConsoleWriter
+        {
+            void WriteText(string text);
+        }
+
+        
+    }
+}
