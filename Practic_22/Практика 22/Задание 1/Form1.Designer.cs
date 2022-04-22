3fb11ea7ce8693ef48c75c4623626a8062fb2f56
f0ff1 <drewcool@mail.ru>
2022-04-22 10:04:42 +0300

    Changet Practic 22
    Изменил практику № 22

--- Practic_22/Практика 22/Задание 1/Form1.Designer.cs
+++ Practic_22/Практика 22/Задание 1/Form1.Designer.cs
@@ -45,7 +45,7 @@ namespace Задание_1
             this.labeX.Depth = 0;
             this.labeX.Font = new System.Drawing.Font("Roboto", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.labeX.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.labeX.Location = new System.Drawing.Point(99, 95);
+            this.labeX.Location = new System.Drawing.Point(12, 75);
             this.labeX.MouseState = MaterialSkin.MouseState.HOVER;
             this.labeX.Name = "labeX";
             this.labeX.Size = new System.Drawing.Size(156, 19);
@@ -58,7 +58,7 @@ namespace Задание_1
             this.materialLabel2.Depth = 0;
             this.materialLabel2.Font = new System.Drawing.Font("Roboto", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.materialLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.materialLabel2.Location = new System.Drawing.Point(99, 142);
+            this.materialLabel2.Location = new System.Drawing.Point(12, 122);
             this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel2.Name = "materialLabel2";
             this.materialLabel2.Size = new System.Drawing.Size(156, 19);
@@ -71,7 +71,7 @@ namespace Задание_1
             this.materialLabel3.Depth = 0;
             this.materialLabel3.Font = new System.Drawing.Font("Roboto", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.materialLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.materialLabel3.Location = new System.Drawing.Point(99, 186);
+            this.materialLabel3.Location = new System.Drawing.Point(12, 166);
             this.materialLabel3.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel3.Name = "materialLabel3";
             this.materialLabel3.Size = new System.Drawing.Size(156, 19);
@@ -80,28 +80,28 @@ namespace Задание_1
             // 
             // BoxX
             // 
-            this.BoxX.Location = new System.Drawing.Point(286, 95);
+            this.BoxX.Location = new System.Drawing.Point(199, 75);
             this.BoxX.Name = "BoxX";
             this.BoxX.Size = new System.Drawing.Size(100, 23);
             this.BoxX.TabIndex = 3;
             // 
             // BoxY
             // 
-            this.BoxY.Location = new System.Drawing.Point(286, 142);
+            this.BoxY.Location = new System.Drawing.Point(199, 122);
             this.BoxY.Name = "BoxY";
             this.BoxY.Size = new System.Drawing.Size(100, 23);
             this.BoxY.TabIndex = 4;
             // 
             // BoxZ
             // 
-            this.BoxZ.Location = new System.Drawing.Point(286, 186);
+            this.BoxZ.Location = new System.Drawing.Point(199, 166);
             this.BoxZ.Name = "BoxZ";
             this.BoxZ.Size = new System.Drawing.Size(100, 23);
             this.BoxZ.TabIndex = 5;
             // 
             // BoxWrite
             // 
-            this.BoxWrite.Location = new System.Drawing.Point(99, 243);
+            this.BoxWrite.Location = new System.Drawing.Point(12, 223);
             this.BoxWrite.Multiline = true;
             this.BoxWrite.Name = "BoxWrite";
             this.BoxWrite.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
@@ -114,7 +114,7 @@ namespace Задание_1
             this.materialFlatButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.materialFlatButton1.Depth = 0;
             this.materialFlatButton1.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.materialFlatButton1.Location = new System.Drawing.Point(288, 413);
+            this.materialFlatButton1.Location = new System.Drawing.Point(201, 393);
             this.materialFlatButton1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.materialFlatButton1.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialFlatButton1.Name = "materialFlatButton1";
@@ -129,7 +129,7 @@ namespace Задание_1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(405, 459);
+            this.ClientSize = new System.Drawing.Size(312, 433);
             this.Controls.Add(this.materialFlatButton1);
             this.Controls.Add(this.BoxWrite);
             this.Controls.Add(this.BoxZ);
