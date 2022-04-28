ab81a1e8216df46383cb7b12f8fb0b542844b8b6
f0ff1 <drewcool@mail.ru>
2022-04-28 13:05:17 +0300

    Add Practic 28
    Добавил практику № 28

---
+++ Practic_28/Практика 28/Задание 2/TestDrawindTwo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+
+namespace Задание_2
+{
+    class TestDrawindTwo : Control
+    {
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            drawingContext.DrawLine(new Pen(Brushes.Red, 2),
+                new Point(0, 0),
+                new Point(this.Height, this.Width));
+
+            drawingContext.DrawLine(new Pen(Brushes.Red, 2),
+                new Point(0, this.Width),
+                new Point(this.Height, 0));
+
+            base.OnRender(drawingContext);
+        }
+    }
+}
