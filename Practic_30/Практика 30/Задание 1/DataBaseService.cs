c9b4b9dfc8a86baf9576804a80f6aac03a5662c6
f0ff1 <drewcool@mail.ru>
2022-04-30 12:45:44 +0300

    Add Practic 30
    Добавил практику № 30

---
+++ Practic_30/Практика 30/Задание 1/DataBaseService.cs
@@ -0,0 +1,184 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_1
+{
+    public class DataBaseService
+    {
+        public static MySqlConnection CreateConnectino()
+        {
+            try
+            {
+                string host = "localhost";
+                int port = 3306;
+                string database = "dbtur_firm";
+                string username = "root";
+                string password = "1234";
+
+                string connString = "Server=" + host + ";Database=" + database +
+                    ";port=" + port + ";User Id=" + username + ";password=" + password;
+
+                return new MySqlConnection(connString);
+            }
+            catch (Exception)
+            {
+
+                throw new Exception("База данных не была подключена!");
+            }
+            
+        }
+
+        public static List<string> GetTours(MySqlConnection myCon)
+        {
+            try
+            {
+                myCon.Open();
+                string sqlTours = "SELECT * FROM tours";
+
+                MySqlCommand command = new MySqlCommand(sqlTours, myCon);
+                MySqlDataReader reader = command.ExecuteReader();
+                var myList = new List<string>();
+
+                while (reader.Read())
+                {
+                    myList.Add($"Город |{reader[1]}| Цена |{reader[2]}|BYN");
+                }
+                reader.Close();
+                myCon.Close();
+
+                return myList;
+            }
+            catch (Exception)
+            {
+                throw new Exception("Туры отсутствуют");
+            }
+            
+        }
+
+        public static List<string> GetTourists(MySqlConnection myCon)
+        {
+            try
+            {
+                myCon.Open();
+                string sqlTourists = "SELECT * FROM tourists";
+
+                MySqlCommand command = new MySqlCommand(sqlTourists, myCon);
+                MySqlDataReader reader = command.ExecuteReader();
+                var myList = new List<string>();
+
+                while (reader.Read())
+                {
+                    myList.Add($"Имя |{reader[1]}| Фамилия |{reader[2]}|");
+                }
+                reader.Close();
+                myCon.Close();
+
+                return myList;
+            }
+            catch (Exception)
+            {
+                throw new Exception("Туристы отсутствуют!");
+            }
+            
+        }
+
+        public static void DeleteTour(MySqlConnection myCon, List<string> selectedTour)
+        {
+            try
+            {
+                myCon.Open();
+                string sqlTours = "DELETE FROM tours WHERE name = @selectedTour";
+
+                MySqlCommand command = new MySqlCommand(sqlTours, myCon);
+
+                command.Parameters.AddWithValue("@selectedTour", selectedTour[1]);
+                command.ExecuteNonQuery();
+
+
+                myCon.Close();
+            }
+            catch (Exception)
+            {
+                throw new Exception("Не выбран тур для удаления");
+            }
+            
+        }
+
+        public static void AddTour(MySqlConnection myCon, string tourName, string tourCost)
+        {
+            try
+            {
+                myCon.Open();
+
+                string sql = "INSERT INTO tours" + "(name, cost) Values(@name, @cost)";
+
+                MySqlCommand command = new MySqlCommand(sql, myCon);
+
+                command.Parameters.AddWithValue("@name", tourName);
+                command.Parameters.AddWithValue("@cost", Convert.ToDecimal(tourCost));
+                command.ExecuteNonQuery();
+
+                myCon.Close();
+            }
+            catch (Exception)
+            {
+                throw new Exception("Не введены данные");   
+            }
+            
+        }
+
+        public static void AddTourist(MySqlConnection myCon, string touristName, string touristSName)
+        {
+            try
+            {
+                myCon.Open();
+
+                string sql = "INSERT INTO tourists" + "(name, secondName) Values(@name, @secondName)";
+
+                MySqlCommand command = new MySqlCommand(sql, myCon);
+
+                command.Parameters.AddWithValue("@name", touristName);
+                command.Parameters.AddWithValue("@secondName", touristSName);
+                command.ExecuteNonQuery();
+
+
+                myCon.Close();
+            }
+            catch (Exception)
+            {
+                throw new Exception("Не введены данные");
+            }
+           
+        }
+
+        public static void ChangeTourist(MySqlConnection myCon, List<string> selectedTourist, string touristNameChange, string touristSNameChange)
+        {
+            try
+            {
+                myCon.Open();
+                string sqlTours = "UPDATE tourists SET name = @name, secondName = @secondName WHERE name = @selectedTourist";
+
+                MySqlCommand command = new MySqlCommand(sqlTours, myCon);
+
+                command.Parameters.AddWithValue("@name", touristNameChange);
+                command.Parameters.AddWithValue("@secondName", touristSNameChange);
+
+                command.Parameters.AddWithValue("@selectedTourist", selectedTourist[1]);
+                command.ExecuteNonQuery();
+
+
+                myCon.Close();
+            }
+            catch (Exception)
+            {
+                throw new Exception("Не выбран турист");
+            }
+            
+        }
+
+    }
+}
---
+++ Practic_30/Практика 30/Задание 1/MainWindow.xaml.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+namespace Задание_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            TextBoxCost.GotFocus += TextBox_GotFocus;
+            TextBoxName.GotFocus += TextBox_GotFocus;
+            TextBoxSecondName.GotFocus += TextBox_GotFocus;
+            
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            var txt = (TextBox)sender;
+            if (txt.Text.Contains("Enter"))
+            {
+                txt.Text = string.Empty;
+            }
+        }
+
+        private void ButtonConnectDB_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var myCon = DataBaseService.CreateConnectino();
+                myCon.Open();
+                ListBoxWriter.Items.Add("DB CONNECTED");
+                myCon.Close();
+            }
+            catch (Exception ex) 
+            {
+                ListBoxWriter.Items.Add(ex.Message);
+            }
+            
+        }
+
+        private void ButtonShowTours_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var myCon = DataBaseService.CreateConnectino();
+                var tours = DataBaseService.GetTours(myCon);
+
+                foreach (var tour in tours)
+                {
+                    ListBoxWriter.Items.Add(tour);
+                }
+            }
+            catch (Exception ex)
+            {
+                ListBoxWriter.Items.Add(ex.Message);
+
+            }
+            
+            
+        }
+
+        private void ButtonShowTourists_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var myCon = DataBaseService.CreateConnectino();
+                var tourists = DataBaseService.GetTourists(myCon);
+
+                foreach (var tourist in tourists)
+                {
+                    ListBoxWriter.Items.Add(tourist);
+                }
+            }
+            catch (Exception ex)
+            {
+                ListBoxWriter.Items.Add(ex.Message);
+            }           
+
+        }
+
+        private void ButtonDeleteTour_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var myCon = DataBaseService.CreateConnectino();
+                var selectedTour = ListBoxWriter.SelectedItem.ToString().Split('|').ToList();
+
+                DataBaseService.DeleteTour(myCon, selectedTour);
+            }
+            catch (NullReferenceException ex)
+            {
+                ListBoxWriter.Items.Add(ex.Message);
+            }
+
+            catch (Exception ex)
+            {
+                ListBoxWriter.Items.Add(ex.Message);
+            }
+            
+            
+
+            
+        }
+
+        private void ButtonAddTour_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var myCon = DataBaseService.CreateConnectino();
+                var tourName = TextBoxName.Text;
+                var tourCost = TextBoxCost.Text;
+
+                DataBaseService.AddTour(myCon, tourName, tourCost);
+            }
+            catch (Exception ex)
+            {
+                ListBoxWriter.Items.Add(ex.Message);
+            }
+            
+   
+        }
+
+        private void ButtonAddTourist_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var myCon = DataBaseService.CreateConnectino();
+                var touristName = TextBoxName.Text;
+                var touristSName = TextBoxCost.Text;
+
+                DataBaseService.AddTourist(myCon, touristName, touristSName);
+            }
+            catch (Exception ex)
+            {
+                ListBoxWriter.Items.Add(ex.Message);                
+            }
+            
+
+        }
+
+        private void ButtonChangeTourist_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var myCon = DataBaseService.CreateConnectino();
+                var touristNameChange = TextBoxName.Text;
+                var touristSNameCnahge = TextBoxSecondName.Text;
+                var selectedTourist = ListBoxWriter.SelectedItem.ToString().Split('|').ToList();
+
+                DataBaseService.ChangeTourist(myCon, selectedTourist, touristNameChange, touristSNameCnahge);
+            }
+            catch (Exception ex)
+            {
+                ListBoxWriter.Items.Add(ex.Message);
+            }
+            
+
+        }
+    }
+}
